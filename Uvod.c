46a26c86305169b4e487404b3f20ad4640cf0faf
NisamInvalid <95847404+NisamInvalid@users.noreply.github.com>
2021-12-09 09:37:36 +0100

    Add files via upload

---
+++ Uvod.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void main(){
+    int stranicaA=80;
+    printf("%d" ,stranicaA);
+}
---
+++ volumen kuboida.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main(){
+int stranicaA=80;
+int stranicaB=50;
+int stranicaC=75;
+int V=stranicaA*stranicaB*stranicaC;
+printf("%d", V);
+}
