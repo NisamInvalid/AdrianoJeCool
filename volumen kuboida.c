3ca824decba930ff1bfd3d11135e1ebc59f9f162
NisamInvalid <95847404+NisamInvalid@users.noreply.github.com>
2021-12-09 10:22:18 +0100

    Add files via upload

--- volumen kuboida.c
+++ volumen kuboida.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 void main(){
-int stranicaA=80;
-int stranicaB=50;
-int stranicaC=75;
-int V=stranicaA*stranicaB*stranicaC;
-printf("%d", V);
+    int stranicaA=80;
+    int stranicaB=50;
+    int stranicaC=75;
+    printf("Unes stranice:\n");
+    scanf("%d %d %d",&stranicaA,&stranicaB,&stranicaC);
+    int V=stranicaA*stranicaB*stranicaC;
+    printf("Volumen je %d cm^3",V);
 }
