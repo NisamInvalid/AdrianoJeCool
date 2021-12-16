6a29fed5cc9ad65004deff1cfbaa1811aa205886
NisamInvalid <95847404+NisamInvalid@users.noreply.github.com>
2021-12-16 08:17:56 +0100

    Add files via upload

---
+++ Relacijski operatori.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+
+
+    printf("6==6 -> %d.\n", (6 == 6));
+    printf("3 > 5 -> %d.\n", (3 > 5));
+    printf("3 < 5 -> %d.\n", (3 < 5));
+    printf("8 != 9 -> %d.\n", (8 != 9));
+    printf("5 >= 5 -> %d.\n", (5 >= 5));
+
+
+
+}
