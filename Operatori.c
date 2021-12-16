328fdcd12ca07c10f454ab4298043a7f97be0a40
NisamInvalid <95847404+NisamInvalid@users.noreply.github.com>
2021-12-16 07:58:24 +0100

    Add files via upload

---
+++ Operatori.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+
+int main()
+{
+    int X;
+    int y;
+    scanf("%d", &x)
+    scanf("%d", &y)
+
+    int z=x*y;
+    int a=x+y;
+    int b=x-y;
+    int c=x/y;
+    printf("zbrajanje %d\n",a);
+    printf("oduzimanje %d\n",b);
+    printf("mnozenje %d\n",z);
+    printf("dijeljenje %d\n",c);
+
+}
