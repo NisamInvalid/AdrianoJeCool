f9c0616523430bb727c60afcfd685d137be25fb9
Adriano <95847404+NisamInvalid@users.noreply.github.com>
2022-02-03 10:00:04 +0100

    Add files via upload

---
+++ Markova zarada.c
@@ -0,0 +1,14 @@
+#include <math.h>
+#include <stdio.h>
+
+int main(){
+
+int markova_zarada;
+int ukupna_placa;
+
+printf("Unesi ukupnu placu u kn: ");
+scanf("%d", &ukupna_placa);
+markova_zarada=ukupna_placa*0.8*0.8*0.8;
+printf("Markova zarada je %d kn", markova_zarada);
+
+}
