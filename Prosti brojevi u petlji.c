4dabbca470768f87f2dc9eb4fe4f2959d3bd4f27
Adriano <95847404+NisamInvalid@users.noreply.github.com>
2022-03-31 08:17:06 +0200

    Add files via upload

---
+++ Prosti brojevi u petlji.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	 int minimum=100, maximum=1000,najveci, jeLiDjeljivSBiloKojim, count=0, i, j;
+	 /* Inputs */
+
+
+	 for(i=minimum; i<=maximum; i++)
+	 {
+		  jeLiDjeljivSBiloKojim = 0;
+		  for(j=2; j <= i/2; j++)
+		  {
+			   if(i%j==0)
+			   {
+				    jeLiDjeljivSBiloKojim=1;
+				    break;
+			   }
+		  }
+		  if(jeLiDjeljivSBiloKojim==0 && i>=2)
+		  {
+		      najveci=i;
+		   	count++;
+		  }
+	 }
+	 printf("\n Prime Count = %d\n Naj je veci %d", count,najveci);
+	 return(0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
