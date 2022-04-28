58e3c305aeee534d3fc7709abf3e700f4950d168
Adriano <95847404+NisamInvalid@users.noreply.github.com>
2022-04-28 10:20:29 +0200

    Add files via upload

--- točni zadaci while petlja.c
+++ točni zadaci while petlja.c
@@ -6,18 +6,23 @@ int main(void)
 int broj_zadataka;
 int zad_1, zad_2, zad_3, zad_4, zad_5;
 int zad_zad;
-int josipovo_znanje;
+int josipovo_znanje,bodovi=0;
 
-printf("Koliko ima ukupno zadataka: \n");
+printf("Koliko ima ukupno zadataka: ");
 scanf("%d", &broj_zadataka);
 
-printf("Koliko Josip zna zadataka: \n");
+printf("Koliko Josip zna zadataka: ");
 scanf("%d", &josipovo_znanje);
 
 printf("Koji su zadaci bili odabrani? ");
 scanf("%d %d %d %d %d", &zad_1, &zad_2, &zad_3, &zad_4, &zad_5);
 
-while(josipovo_znanje)
-
+while(josipovo_znanje){
 scanf("%d", &zad_zad);
-    if(zad_zad==zad_1||zad_zad==zad_2||zad_zad==zad_3||zad_zad==zad_4||zad_zad==zad_5)
+josipovo_znanje--;
+    if(zad_zad==zad_1||zad_zad==zad_2||zad_zad==zad_3||zad_zad==zad_4||zad_zad==zad_5) bodovi++;
+}
+int ocjena=bodovi==0?1:bodovi;
+
+printf("Ocjena je %d",ocjena);
+}
