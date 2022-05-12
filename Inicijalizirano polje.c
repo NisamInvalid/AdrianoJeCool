ed2f747badde6f60299269e25b441bc80102b5f7
Adriano <95847404+NisamInvalid@users.noreply.github.com>
2022-05-12 10:17:32 +0200

    Add files via upload

---
+++ Inicijalizirano polje.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+
+int x[6];
+
+for(int i=0;i<6;i++){
+    printf("Unesi broj: ");
+    scanf("%d", &x[i]);
+}
+printf("\n");
+
+for(int i=0;i<6;i++){
+    printf("%d\n", x[i]);
+}
+
+
+
+
+
+}
